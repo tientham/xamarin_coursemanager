e5c9de00da8b97d2cb106c3b0a9586fc7afa89c6
Minh Tien TO (TienTham) <tien.tominh@gmail.com>
2020-07-27 23:20:23 +0200

    Login page view

--- coursemanager/App.xaml.cs
+++ coursemanager/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using coursemanager.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace coursemanager
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
