8962fb7bf8d623cb766666766f5017bd2b4e8edc
Minh Tien TO (TienTham) <tien.tominh@gmail.com>
2020-08-01 21:45:46 +0200

    finalize for video part

--- coursemanager/ViewModels/SectionDetailPageViewModel.cs
+++ coursemanager/ViewModels/SectionDetailPageViewModel.cs
@@ -1,15 +1,53 @@
 ﻿using System;
+using System.Windows.Input;
+using LibVLCSharp.Shared;
+using Xamarin.Forms;
+
 namespace coursemanager.ViewModels
 {
     public class SectionDetailPageViewModel : BaseViewModel
     {
+        private LibVLC _libVLC;
+
         public SectionDetailPageViewModel(SectionViewModel section)
         {
             this.title = section.Title;
             this.Transcript = section.Transcript;
+            this.imageVisible = true;
+
+            Core.Initialize();
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC)
+            {
+                Media = new Media(_libVLC, new Uri(section.VideoUrl))
+            };
+
+            PlayVideoCommand = new Command(PlayVideoCommandExecute);
         }
 
         #region PROPERTIES
+
+        public ICommand PlayVideoCommand { get; }
+
+        private MediaPlayer _mediaPlayer;
+        public MediaPlayer MediaPlayer
+        {
+            get => _mediaPlayer;
+            set
+            {
+                SetProperty(ref _mediaPlayer, value);
+            }
+        }
+
+        private bool imageVisible;
+        public bool ImageVisible
+        {
+            get => imageVisible;
+            set
+            {
+                SetProperty(ref imageVisible, value);
+            }
+        }
 
         private string title;
         public string Title
@@ -29,6 +67,21 @@ namespace coursemanager.ViewModels
             {
                 SetProperty(ref transcript, value);
             }
+        }
+
+        #endregion
+
+        #region
+
+        private void PlayVideoCommandExecute(object obj)
+        {
+            ImageVisible = false;
+            _mediaPlayer.Play();
+        }
+
+        public void StopVideo()
+        {
+            _mediaPlayer.Stop();
         }
 
         #endregion
--- coursemanager/Views/SectionDetailPage.xaml.cs
+++ coursemanager/Views/SectionDetailPage.xaml.cs
@@ -9,42 +9,23 @@ namespace coursemanager.Views
     // Free public test video: https://gist.github.com/jsturgis/3b19447b304616f18657
     public partial class SectionDetailPage : ContentPage
     {
-        LibVLC _libVLC;
-        MediaPlayer _mediaPlayer;
-        string _videoUrl;
-
         public SectionDetailPage(SectionViewModel section)
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, true);
             BindingContext = new SectionDetailPageViewModel(section);
             Title = $"Lecture {section.Id}";
-            _videoUrl = section.VideoUrl;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
-            Core.Initialize();
-            _libVLC = new LibVLC();
-            _mediaPlayer = new MediaPlayer(_libVLC)
-            {
-                Media = new Media(_libVLC, new Uri(_videoUrl))
-            };
-            videoView.MediaPlayer = _mediaPlayer;
         }
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
-            _mediaPlayer.Stop();
-        }
-
-        void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
-        {
-            playImage.IsVisible = false;
-            _mediaPlayer.Play();
+            (BindingContext as SectionDetailPageViewModel).StopVideo();
         }
     }
 }
